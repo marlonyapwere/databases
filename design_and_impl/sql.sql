ab7ab61c15b03cf78840803f9f2e978fe3490dd9
marlone-nyapwere <marloneenyapwere@gmail.com>
2024-03-19 15:38:40 +0200

    create user_profile_table

---
+++ design_and_impl/sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS user_profile (
+    id BIGSERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    gender TEXT CHECK(gender IN ('MALE', 'FEMALE')) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
